1bff3756050527de6013a84645f7e459917bf69c
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-06-19 12:37:51 +0300

    setup taiwind css for styling

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// import "./App.css";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-purple-800">Main App File</h1>
+    </>
+  );
+}
+
+export default App;
